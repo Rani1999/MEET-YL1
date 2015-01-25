736dab3a98de6102651e494b16fc3afbcb34fe03
Rani Gerges <rani16@meet.mit.edu>
2015-01-25 19:52:29 +0200

    OOP

--- divisors.py
+++ divisors.py
@@ -2,3 +2,4 @@ n= input("enter a number")
 x=int(n)
 for y in range(1,x+1):
 	print (y)
+
---
+++ oop.py
@@ -0,0 +1,22 @@
+class Animal:
+	def __init__(self, name, age, color, size):
+		self.name=name
+		self.age=age
+		self.color=color
+		self.size=size
+	def print_all(self):
+		print(self.name)
+		print(self.age)
+		print(self.color)
+		print(self.size)
+
+	def sleep(self, hours, dream):
+		print("the Animal ",self.name," is sleeping for ",hours , "of ", dream)
+		
+
+
+dog = Animal("sadek", 22, "yellow", "tiny")
+lion = Animal("nofar", 1, "orange", "big")
+dog.print_all()
+lion.print_all()
+dog.sleep(23,"python")
